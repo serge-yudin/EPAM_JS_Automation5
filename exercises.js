2b84fd203d23be7dbd2a353e37826304b38e9821
Serge Yudin <vodprava.com@gmail.com>
2021-06-29 13:00:32 +0200

    js code added

---
+++ exercises.js
@@ -0,0 +1,9 @@
+//Создать 2 объекта: animal и cat, объект animal добавить свойство move, объект cat должен наследовать свойство move
+
+let animal = {};
+let cat = {};
+
+animal.move = true;
+cat.__proto__ = animal;
+
+console.log(cat.move);
